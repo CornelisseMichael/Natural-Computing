e2566eba563d3c776e6c18244fb1b951455f7c97
MiekevdMeiden <50616839+MiekevdMeiden@users.noreply.github.com>
2025-05-07 14:36:46 +0200

    new branch for fire model development

--- Project/forest_fire_adjustments.py
+++ Project/forest_fire_adjustments.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib; matplotlib.use("TkAgg") #this line makes the animation run in the IDE. Comment if you only want to export it
+import matplotlib; matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 from matplotlib import animation
 from matplotlib import colors
@@ -24,28 +24,38 @@ bounds = [0,1,2,3,4]
 norm = colors.BoundaryNorm(bounds, cmap.N)
 
 
-# load in the map
-image_path = 'testroom.bmp'
-image = Image.open(image_path)
-image_array = np.array(image)
+def initialize_grid(fire_source, img_path=None):
+    if img_path is not None:
+        image = Image.open(img_path)
+        image_array = np.array(image)
+        mapping = {0: 3, 1: 1}
+        mapped_array = np.vectorize(mapping.get)(image_array)
 
-mapping = {0: 3, 1: 0}
-mapped_array = np.vectorize(mapping.get)(image_array)
+        print(mapped_array)
+        grid = mapped_array
+    else: # if no image was given, initialize an empty grid of 100x100
+        ny, nx = 100, 100
+        grid = np.zeros((ny, nx))
+        grid[1:ny - 1, 1:nx - 1] = np.random.randint(0, 2, size=(ny - 2, nx - 2))
+        grid[1:ny - 1, 1:nx - 1] = np.random.random(size=(ny - 2, nx - 2)) < forest_fraction
 
-print(mapped_array)
+    fx, fy = fire_source
+    grid[fx, fy] = FIRE
+    return grid
 
-def iterate(X):
+def iterate(X, i):
     """Iterate the forest according to the forest-fire rules."""
 
     # The boundary of the forest is always empty, so only consider cells
     # indexed from 1 to nx-2, 1 to ny-2
+    # print(i)
     X1 = np.zeros((ny, nx))
     for ix in range(1,nx-1):
         for iy in range(1,ny-1):
             if X[iy,ix] == WALL:
                 X1[iy,ix] = WALL
-            if X[iy,ix] == EMPTY and np.random.random() <= p:
-                X1[iy,ix] = TREE
+            # if X[iy,ix] == EMPTY and np.random.random() <= p:
+            #    X1[iy,ix] = TREE
             if X[iy,ix] == TREE:
                 X1[iy,ix] = TREE
                 for dx,dy in neighbourhood:
@@ -59,24 +69,35 @@ def iterate(X):
                 else:
                     if np.random.random() <= f:
                         X1[iy,ix] = FIRE
+            if X[iy,ix] == FIRE:
+                if np.random.random() <= 0.005:
+                    X1[iy,ix] = EMPTY
+                else:
+                    X1[iy,ix] = FIRE
+                for dx,dy in neighbourhood:
+                    if X[iy+dy,ix+dx] == EMPTY:
+                        if np.random.random() <= 0.05:
+                            X1[iy,ix] = EMPTY
+            if X[iy,ix] == EMPTY:
+                for dx,dy in neighbourhood:
+                    if X[iy + dy, ix + dx] == FIRE and np.random.random() <= 0.02:
+                        X1[iy,ix] = FIRE
+
     return X1
 
 # The initial fraction of the forest occupied by trees.
 forest_fraction = 0.2
 wall_fraction = 0.3
 # Probability of new tree growth per empty cell, and of lightning strike.
-p, f = 0.05, 0.0001
-# Forest size (number of cells in x and y directions).
-nx, ny = 100, 100
-# Initialize the forest grid.
-X  = np.zeros((ny, nx))
-X[1:ny-1, 1:nx-1] = np.random.randint(0, 2, size=(ny-2, nx-2))
-X[1:ny-1, 1:nx-1] = np.random.random(size=(ny-2, nx-2)) < forest_fraction
-#X[1:ny-1, 1:nx-1] = np.where(np.random.random(size=(ny-2, nx-2)) < wall_fraction, 3, 0)
-#X[1:ny-1, 1:nx-1] = np.random.random(0, 3, size=(ny-2, nx-2)) < wall_fraction
-print(X)
+p, f = 0.05, 0.000
+d = 0.01
 
-X = mapped_array
+# load in the map
+image_path = 'testroom.bmp'
+X = initialize_grid((25, 50), image_path)
+ny, nx = X.shape
+
+
 
 fig = plt.figure(figsize=(25/3, 6.25))
 ax = fig.add_subplot(111)
@@ -87,15 +108,16 @@ im = ax.imshow(X, cmap=cmap, norm=norm)#, interpolation='nearest')
 # The animation function: called to produce a frame for each generation.
 def animate(i):
     im.set_data(animate.X)
-    animate.X = iterate(animate.X)
+    animate.X = iterate(animate.X, i)
 # Bind our grid to the identifier X in the animate function's namespace.
 animate.X = X
 
 # Interval between frames (ms).
 interval = 100
-anim = animation.FuncAnimation(fig, animate, interval=interval, frames=1)
-#anim.save("forest_fire.mp4")
-html_out = anim.to_jshtml()
-with open('anim.html', "w") as tf:
-    tf.write(html_out)
+anim = animation.FuncAnimation(fig, animate, interval=interval, frames=200)
 plt.show()
+
+#anim.save("forest_fire.mp4")
+# html_out = anim.to_jshtml()
+# with open('anim.html', "w") as tf:
+#     tf.write(html_out)
