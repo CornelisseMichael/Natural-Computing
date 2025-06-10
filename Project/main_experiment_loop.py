c67596e15bdd2a9b93f93b508d5e8a43e833e9f7
Michael Cornelisse <michaelcornelisse88@gmail.com>
2025-06-10 13:58:30 +0200

    added main with loop

---
+++ Project/main_experiment_loop.py
@@ -0,0 +1,80 @@
+# You’ll need display() to render in Colab/Jupyter
+from IPython.display import display, HTML, Image
+from structures import Environment
+from structures import StructureLayer
+from fireSimulation import FireLayer, SmokeLayer
+from aids import LightStripLayer, FireAlarmLayer
+import matplotlib ; matplotlib.use("TkAgg") #to run the animations in PyCharm
+import matplotlib.pyplot as plt
+from mapLoading import *
+import numpy as np
+from EvaluationMetrics import Evaluation
+from FireAlarm_config import get_firealarm_config
+import random
+
+
+if __name__ == "__main__":
+    # build & seed
+    filepath = './maps/obstacles_2.png'
+
+    floormap = loadFromImage(filepath)
+    height, width = floormap.shape
+    
+    # Generate seeds randomly or define them manually
+    # Generate 10 unsigned-32-bit seeds (0 through 2**32−1)
+    seed_range = 2
+    random.seed(42)
+    seeds = [random.randint(0, 10000) for _ in range(seed_range)]
+    print(seeds)
+    
+    evacuee_densities = ["small", "medium", "large"]
+    
+    scenarios = []
+    
+    for seed in seeds:
+        for density in evacuee_densities:
+            env = Environment(width, height).set_seed(seed)
+            struct = StructureLayer(width, height)
+            struct.grid = floormap.tolist()
+            env.add_layer('structure', struct)
+
+            fire = FireLayer(width,height, p_ignite=0.5, burn_time=10, spread_interval=2)
+            env.add_layer('fire', fire)
+            env.ignite_fire_randomly(n=1)
+            
+            smoke = SmokeLayer(width,height, diff_rate=0.1, emit_rate=0.4)
+            env.add_layer('smoke', smoke)
+
+            exits = env.get_exits()
+            print(exits)
+            light = LightStripLayer(width, height, exits)
+            env.add_layer('light', light)
+
+            # ==== config settings ==== #
+            # Select from baseline, threedoors, obstacles, offices for the first string
+            # Select from from floor placement type located in FireAlarm_config
+            config = get_firealarm_config('obstacles', 'one_exp3')
+            
+            # Initializing FireAlarmLayer
+            firealarm = FireAlarmLayer(width, height, firealarm_coords=config['coords'], radius=config['radius']) 
+            env.add_layer('firealarm', firealarm)
+
+            #env.spawn_agents_randomly(27)
+            env.spawn_agents(density=density)
+            env.save_initial_state()
+
+            # Evaluator
+            evaluator = Evaluation(env)
+
+            # animation
+            anim = env.animate(steps=1000, interval=100, evaluator= evaluator)
+            plt.show() # to show the animation in your IDE (pycharm)
+
+
+            display(anim)
+            #display(HTML(anim.to_jshtml()))
+            #anim.save('evac-1.gif', writer='pillow', fps=5)
+
+            print(evaluator.report()) 
+            
+        
